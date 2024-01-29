ea62766cb42b046ed79852a60f841f22c62f7a96
Igor Kaseko <igorksk@gmail.com>
2024-01-29 11:51:14 +0200

    Add filter button

--- src/SearchList.tsx
+++ src/SearchList.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import { DATA, Person } from "./CustomersDB";
 
 const SearchList: React.FC = () => {
+
+  const [customersList, ] = useState<Person[]>(DATA);
+  const [filteredCustomersList, setFilteredCustomersList] = useState<Person[]>(DATA);
+
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+    const handleSearch = () => {
+      if (searchTerm === '') {
+        setFilteredCustomersList(customersList);
+      } else {
+        setFilteredCustomersList(customersList.filter((c) => c.name === searchTerm))
+      }
+    };
+
     // Function to map data to HTML list
     const mapDataToHtmlList = (data : Person[]): JSX.Element[] => {
       return data.map((item, index) => (
@@ -8,11 +23,18 @@ const SearchList: React.FC = () => {
       ));
     };
   
-    return (
+    return (       
       <div>
+        <input
+          type="text"
+          placeholder="Enter your search term"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
         <h2>List of Items</h2>
         <ul>
-          {mapDataToHtmlList(DATA)}
+          {mapDataToHtmlList(filteredCustomersList)}
         </ul>
       </div>
     );
